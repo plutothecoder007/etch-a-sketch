0e354b4d33287ec808b67d76f1dd3e7234b2cfea
plutothecoder007 <vitaliykrasovskiy2001@gmail.com>
2024-11-13 12:27:26 +0100

    Made create div grid function and set grid to 16x16 by default.

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-const container = document.querySelector(".container");     //Makes the main container
+const container = document.querySelector(".container");     //Refers to the container in a variable.
 container.style.width = "700px";
 container.style.height = "700px";
+container.style.display = "grid";
 
-function makeGrid () {          
-    let size = prompt("Pick your grid size", 'Type grid size here.');
+function createDivGrid (num) {
+    let cellSize = 700 / num;       //Calculates the cell size so they all fit within the container.
 
-        if (size > 100) {
-        prompt("That's too big. Choose a size under 100x100","Type grid size here.");
-        } 
+    container.style.gridTemplateColumns = `repeat(${num}, ${cellSize}px)`;  //Set the size of columns based on cell size.
+    container.style.gridTemplateRows = `repeat(${num}, ${cellSize}px)`;
+
+
+
+    for (let i = 0; i < num * num; i++) {
+        const div = document.createElement("div");
+        div.style.width = `${cellSize}px`;
+        div.style.height = `${cellSize}px`;
+        div.style.border = "0.5px solid black";
+        
+        container.appendChild(div);
+    }
+
 }
 
-function createDivs (num) {
-    let width = Number(num);
-    let height = Number(num);
-    const div = document.createElement("div");
-    div.style.width = amount;
+createDivGrid(16)       //Loads a 16x16 grid by deafult.
 
+
+function makeNewGrid () {          
+    let size = prompt("Pick your grid size", 'Type grid size here.');       //size variable takes value from user input.
+
+        if (size > 100) {
+            prompt("That's too big. Choose a size under 100x100","Type grid size here.");
+        } else if (size <= 0) {
+            prompt("Grid size can't be negative", 'Type grid size here');
+        }
+        else {
+            createDivGrid(size)
+        }
 }
 
